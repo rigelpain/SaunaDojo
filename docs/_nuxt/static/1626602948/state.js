9d899c235150560d40a4ba31423453caec555b83
Haruaki Kobayashi <rigelpain@gmail.com>
2021-07-18 19:10:42 +0900

    [update] ver.0.1.2

---
+++ docs/_nuxt/static/1626602948/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsaunadojo\u002F_nuxt\u002Fstatic\u002F1626602948",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsaunadojo\u002F",assetsPath:"\u002Fsaunadojo\u002F_nuxt\u002F",cdnURL:a}}}}(null));
